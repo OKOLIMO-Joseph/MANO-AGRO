6546afb53c1f99f7df086b199ae69ad9a50db0b8
OKOLIMO-Joseph <okolimofficial@gmail.com>
2024-02-09 15:19:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import About from './About';
+// import './App.css';
+// import Hommee from './Home';
+// import { Switch } from '@mui/material';
+import Header from './Header';
+import { Desc } from './Pages/Description';
+// import SwipeableTextMobileStepper from './Pages/SlideShow';
+import Products from './Slider';
+// import HomPage from './HomePage';
+// import ImageList from './Slider';
+import { Routes, Route } from 'react-router-dom';
+
+// import PersistentDrawerLeft from "./Pages/FirstPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      {/* <Desc /> */}
+      {/* <BrowserRouter> */}
+      <Routes>
+        {/* <Switch> */}
+                 {/* <PersistentDrawerLeft /> */}
+      {/* <Hommee /> */}
+      <Route path='/' element={<Desc />} />
+      {/* <Products /> */}
+      {/* <SwipeableTextMobileStepper />
+      <Desc /> */}
+      {/* <HomPage /> */}
+      <Route path='/home' element={<Products />} />
+      {/* <About /> */}
+        {/* </Switch> */}
+     </Routes>
+      {/* </BrowserRouter> */}
     </div>
   );
 }
